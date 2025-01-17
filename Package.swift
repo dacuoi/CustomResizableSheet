4b898eece8aeb69a894e45a4f89c47626b1caaec
dacuoi <paintbsd@gmail.com>
2025-01-17 11:43:44 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "ResizableSheet",
+            name: "CustomResizableSheet",
             targets: ["ResizableSheet"]
         ),
     ],
